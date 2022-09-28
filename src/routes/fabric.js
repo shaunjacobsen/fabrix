cc537d93648e987c4bbc7902d61f72481e3617a6
Shaun Jacobsen <shaun@sjacobsen.com>
2022-09-28 20:08:50 +0200

    Adds fabrics route

--- src/routes/fabric.js
+++ src/routes/fabric.js
@@ -1,28 +1,15 @@
 const express = require('express');
+const { Fabric } = require('../models');
 
 const { authenticate, permit } = require('./../middleware/authenticate');
 
 module.exports = app => {
-  app.get(
-    '/fabric/:id',
-    authenticate,
-    permit('user', 'admin'),
-    async (req, res) => {
-      try {
-        const flight = await models.flight.findById(req.params.id);
-        const trip = await models.trip.findById(flight.trip_id);
-        if (trip.isUserAuthorizedToView(req.user)) {
-          const flights = await flight.markAsSelected();
-          if (flights.error) {
-            res.status(400).json({ error: flights.error });
-          }
-          res.json(filterFlightGroupData(flights));
-        } else {
-          res.status(401).send();
-        }
-      } catch (e) {
-        res.status(400).json(e);
-      }
+  app.get('/fabrics', authenticate, permit('user', 'admin'), async (req, res) => {
+    try {
+      const fabrics = await Fabric.findAll();
+      res.json(fabrics);
+    } catch (e) {
+      res.status(400).json(e.message);
     }
-  );
+  });
 };
